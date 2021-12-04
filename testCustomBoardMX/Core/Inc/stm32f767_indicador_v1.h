3bf2d3bf972b9feeea521eb9c5992d00890fb651
gcasti <gcasti_17@yahoo.com.ar>
2021-12-04 18:09:21 -0300

    Proyecto que maneja el ADC configurado parcialmente con CubeMX
    Configura el SPI2 basado en el CubeMX y la interrupción del pin DRDY
    cada vez que se tiene un dato.
    Se utiliza en el reloj interno
    Debe implementarse la lectura del dato del ADC

---
+++ testCustomBoardMX/Core/Inc/stm32f767_indicador_v1.h
@@ -0,0 +1,49 @@
+/*
+ * stm32f767_indicador_v1.h
+ *
+ *  Created on: 1 dic. 2021
+ *      Author: gcasti
+ */
+
+#ifndef BSP_STM32F767_INDICADOR_V1_STM32F767_INDICADOR_V1_H_
+#define BSP_STM32F767_INDICADOR_V1_STM32F767_INDICADOR_V1_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f7xx_hal.h"
+#include <stdbool.h>
+
+
+#define BUZZER_PORT GPIOC
+#define BUZZER_PIN GPIO_PIN_10
+
+#define AD_TEMP_PIN GPIO_PIN_15
+#define AD_TEMP_GPIO_PORT GPIOB
+#define AD_A0_PIN GPIO_PIN_12
+#define AD_A0_GPIO_PORT GPIOB
+#define AD_PDWN_PIN GPIO_PIN_15
+#define AD_PDWN_GPIO_PORT GPIOD
+#define AD_SPEED_PIN GPIO_PIN_2
+#define AD_SPEED_GPIO_PORT GPIOG
+#define AD_GAIN0_PIN GPIO_PIN_3
+#define AD_GAIN0_GPIO_PORT GPIOG
+#define AD_GAIN1_PIN GPIO_PIN_4
+#define AD_GAIN1_GPIO_PORT GPIOG
+#define AD_EXTCLK_PIN GPIO_PIN_9
+#define AD_EXTCLK_GPIO_PORT GPIOC
+#define AD_DRDY_PIN GPIO_PIN_14
+#define AD_DRDY_GPIO_PORT GPIOB
+
+
+
+SPI_HandleTypeDef hspi2;
+
+
+void BSP_Buzzer_Init(void);
+void BSP_BuzzerBeep(uint32_t period);
+extern void BSP_BuzzerStop();
+
+
+// Configura el ADC
+void BSP_AdcInit(void);
+
+#endif /* BSP_STM32F767_INDICADOR_V1_STM32F767_INDICADOR_V1_H_ */
